85f62082199aceea06bcd178c3dbcfa29eccb486
fujisawa-shin <fujisawa-shin@kayac.com>
2016-08-23 23:27:47 +0900

    リファクタ・ポーズ再生のきりかえ

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,18 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var video = document.querySelector('.js-video');
-
 window.navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 
+var el = document.querySelector('.js-video');
+
 window.navigator.getUserMedia({ video: true }, function (localMediaStream) {
-    video.src = window.URL.createObjectURL(localMediaStream);
-    video.onloadedmetadata = function (e) {
-        // Ready to go. Do some stuff.
+    el.src = window.URL.createObjectURL(localMediaStream);
+    el.onloadedmetadata = function (e) {
+        // TODO: 描画状況・透明ピクセルを計測して、アスペクト比出して、いい具合にトリミング
+
+        el.addEventListener('click', function () {
+            el.paused ? el.play() : el.pause();
+        });
     };
 }, function () {
     alert('fail.');
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
-const video = document.querySelector('.js-video');
-
 window.navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 
+const el = document.querySelector('.js-video');
+
 window.navigator.getUserMedia({ video: true }, (localMediaStream) => {
-    video.src = window.URL.createObjectURL(localMediaStream);
-    video.onloadedmetadata = (e) => {
-        // Ready to go. Do some stuff.
+    el.src = window.URL.createObjectURL(localMediaStream);
+    el.onloadedmetadata = (e) => {
+        // TODO: 描画状況・透明ピクセルを計測して、アスペクト比出して、いい具合にトリミング
+
+        el.addEventListener('click', () => {
+            el.paused ? el.play() : el.pause();
+        });
     };
 }, () => {
     alert('fail.');
